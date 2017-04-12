95a29589af8fc8336d5c5a2b116a0c443e76e448
root <abderrezak.khif@gmail.com>
2017-04-12 04:35:36 +0200

    First commit

--- Recommand/views.py
+++ Recommand/views.py
@@ -3,10 +3,17 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import *
 
 # Create your views here.
 
 
 @login_required(login_url='/index/')
-def list(request):
+def list(request,user_id):
+    '''
+    :param request: 
+    :return: une vue ou l'utilisateur aura accès a tout les films qu'il a déja regarder et noter et les films 
+    qui lui sont recommandé
+    '''
+    print(user_id)
     return render(request, 'log.html')
