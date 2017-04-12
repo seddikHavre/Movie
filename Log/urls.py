32d712ce6ae4d7c4cc8cc2456f5ed527c1e9b61e
root <abderrezak.khif@gmail.com>
2017-04-12 03:04:00 +0200

    First commit

--- Log/urls.py
+++ Log/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
+    url(r'^index/$', views.index, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
-    url(r'^log/$', views.profile, name='profile'),
+    #url(r'^log/$', views.profile, name='profile'),
 ]
